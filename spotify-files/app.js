dd490c6610ad9c7d3861b1c9c3dd6cd991c2afea
erlynnehazey <erlynneong.2020@scis.smu.edu.sg>
2021-11-10 17:11:39 +0800

    commented out firebase in spotify-files app.js

--- spotify-files/app.js
+++ spotify-files/app.js
@@ -1,33 +1,33 @@
 //// FIREBASE COMPONENT //////
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js"; //initialize firebase app
-import { getDatabase, ref, set, child, update, remove, get } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js"
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js"; //initialize firebase app
+// import { getDatabase, ref, set, child, update, remove, get } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js"
 
 
-var client_id,client_secret
+// var client_id,client_secret
 
-const app = initializeApp(firebaseConfig);
-// Get a reference to the database service
-const database = getDatabase(app);
-var reference =ref(realdb); // Refer to realtime DB
-    get(child(reference, "users/clientId" )) // Get the ClientId
-    .then((snapshot)=>{
-        if(snapshot.exists()){
-            if(snapshot.exists()){
-               client_id= snapshot.val()
-            }
-        }
-    })
-    .catch(err=>console.log(err.message))
+// const app = initializeApp(firebaseConfig);
+// // Get a reference to the database service
+// const database = getDatabase(app);
+// var reference =ref(realdb); // Refer to realtime DB
+//     get(child(reference, "users/clientId" )) // Get the ClientId
+//     .then((snapshot)=>{
+//         if(snapshot.exists()){
+//             if(snapshot.exists()){
+//                client_id= snapshot.val()
+//             }
+//         }
+//     })
+//     .catch(err=>console.log(err.message))
 
-    get(child(reference, "users/client_secret" )) // Get the ClientId
-        .then((snapshot)=>{
-            if(snapshot.exists()){
-                if(snapshot.exists()){
-                client_secret= snapshot.val()
-                }
-            }
-        })
-    .catch(err=>console.log(err.message))
+//     get(child(reference, "users/client_secret" )) // Get the ClientId
+//         .then((snapshot)=>{
+//             if(snapshot.exists()){
+//                 if(snapshot.exists()){
+//                 client_secret= snapshot.val()
+//                 }
+//             }
+//         })
+//     .catch(err=>console.log(err.message))
 
     
 
