14da15e2d47e4a946918cca43612ac56268b2827
jacky-teo <jackytjq@outlook.com>
2021-11-12 18:12:47 +0800

    Revamped Upload.js and HTML IT FULLY WORKS

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,5 +1,38 @@
-import {getAllDataOnce} from "./firebase.js"
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js"; //initialize firebase app
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-storage.js" //for firebase storage
+import {getDatabase, ref,set,child,update,remove,get, onValue} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js"
+import { getAuth, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js"
 
+//get user ID from session storage
+const id = sessionStorage.getItem('userID');
+
+// Initiate Firebase Instance
+const firebaseConfig = {
+    apiKey: "AIzaSyDRVQ7r6TGsQhZGvVIXws7y5PTPqlvC2yo",
+    authDomain: "audiophile-eff2c.firebaseapp.com",
+    databaseURL: "https://audiophile-eff2c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "audiophile-eff2c",
+    storageBucket: "audiophile-eff2c.appspot.com",
+    messagingSenderId: "141435951049",
+    appId: "1:141435951049:web:6308bd4b9fe95fb49bba18",
+    measurementId: "G-LKVP0JH4YH"
+};
+const app = initializeApp(firebaseConfig);
+let files,
+    songTitle = document.getElementById('songTitle'),
+    artist = document.getElementById('artist'),
+    ext= document.getElementById('extension'),
+    pbar = document.getElementById('pbar'),
+    upload = document.getElementById('upload'),
+    player = document.getElementById('music'),
+    filename =document.getElementById('filename'),
+    musicSelection = document.getElementById("musicList"),
+    reader = new FileReader()
+
+
+//////////////////////
+// Drag and Drop JS //
+//////////////////////
 document.querySelectorAll(".drop-input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
   dropZoneElement.addEventListener("click", (e) => {
@@ -19,13 +52,117 @@ document.querySelectorAll(".drop-input").forEach((inputElement) => {
     });
   });
   dropZoneElement.addEventListener("drop", (e) => {
-    e.preventDefault();
-    const datatransfer = e.dataTransfer;
-    files = datatransfer.files;
-    var extention = GetFileExt(files[0])
+    e.preventDefault(); // Prevent Event's Auto
+    const data = e.dataTransfer; //Save event's file
+    files = data.files; // Assign data's files to file
+    var extension = GetFileExt(files[0]) // Get File extension
     var name = GetFileName(files[0])
-    nameBox.value=name
-    extlab.innerHTML = extention;
-    reader.readAsDataURL(files[0])
+    filename.innerHTML=name // This functions actaully no need le is just to get extension
+    ext.innerHTML =extension;
+    reader.readAsDataURL(files[0])//Read the current file as a URL
   });
 });
+reader.onload = function(){
+    music.src = reader.result;
+}
+
+///////////////////////
+/// GET FILE NAMES ////
+///////////////////////
+function GetFileExt(file){
+    var temp = file.name.split('.');
+    var ext = temp.slice(temp.length-1,temp.length);
+    return '.'+ext[0]
+}
+function GetFileName(file){
+    var temp = file.name.split('.');
+    var fname = temp.slice(0,-1).join('.');
+    return fname
+}
+
+///////////////////////
+///// UPLOAD FILE /////
+///////////////////////
+
+function validateName(){
+        var regex=/[\.#$\[]]/
+        return !(regex.test(songTitle.value+ '-' +artist.value ));
+    }
+
+async function UploadProcess(){
+    var fileToUpload = files[0]
+    var fileToUploadName = songTitle.value + ' - ' +artist.value;
+     if(!validateName()){
+        alert('Name cannot contain "[.#$[]]" ')
+        return;
+    }
+    const metaData = {
+        contentType: fileToUpload.type
+    }
+    const storage = getStorage();
+    const storageRef =sRef(storage, "users/" + id +"/"+ fileToUploadName);
+    const UploadTask = uploadBytesResumable(storageRef,fileToUpload,metaData);
+    UploadTask.on('state-changed',(snapshot)=>{
+        var progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+        pbar.innerHTML ='Upload ' + progress.toFixed(2) +  "%"
+        pbar.style.width= progress;+"%"
+    },
+    (error)=>{
+        alert("Upload Failed Please Try Again"); // Failed to upload Errror
+    },
+    ()=>{
+        getDownloadURL(UploadTask.snapshot.ref)
+        .then((downloadURL)=>{
+            SaveURLtoRealTimeDB(downloadURL);
+            })
+        .catch(err=>{
+                alert("Upload Failed Please Try Again")
+            })
+            });
+    }
+
+/////////////////////////////////
+///// SAVE FILE TO REALTIME /////
+/////////////////////////////////
+const realdb = getDatabase();
+    // Function REALTIME DATABASE
+    function SaveURLtoRealTimeDB(URL){
+        var name = songTitle.value +  artist.value;
+        var ext =extlab.innerHTML
+        
+        set(ref(realdb,"users/" + id  +"/"+ name),{
+            musicName:(name + ext),
+            musicURL:URL
+        })
+        .then(res=>{
+            alert('Upload Complete')
+        })
+        .catch(err=>{
+            alert('Upload Faile')
+        })
+    }
+///////////////////////////////////////
+///// RETRIEVE ALL FROM REALTIME //////
+///////////////////////////////////////
+function getAllData(){
+        var dbRef =ref(realdb); // Refer to realtime DB
+        get(child(dbRef,"users/" + id ))
+        .then((snapshot)=>{
+            var musicList = [];
+            snapshot.forEach(childSnapshot=>{
+                musicList.push(childSnapshot.val());
+            });
+            addToMusicList(musicList)
+        })
+        .catch(err=>{
+            alert('Failed to retrieve information please try again')
+        })
+}
+///////////////////////////////////////////
+/////////// Create A DROPDOWN /////////////
+///////////////////////////////////////////
+function addToMusicList(musicData){
+    for(let info of musicData){
+        musicSelection.innerHTML += "<option value='" + info.musicURL+ "'>"+ info.musicName+"</option>"
+    }
+}
--- uploads/scripts/upload.js
+++ uploads/scripts/upload.js
@@ -6,38 +6,7 @@ import { getAuth, createUserWithEmailAndPassword, signOut } from "https://www.gs
 //get user ID from session storage
 const id = sessionStorage.getItem('userID');
 
-
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-
-// const user = Vue.createApp({
-//     data() {
-//         return {
-
-//         }
-//     },
-//     created() {
-//         const firebaseConfig = {
-//             apiKey: "AIzaSyDRVQ7r6TGsQhZGvVIXws7y5PTPqlvC2yo",
-//             authDomain: "audiophile-eff2c.firebaseapp.com",
-//             databaseURL: "https://audiophile-eff2c-default-rtdb.asia-southeast1.firebasedatabase.app",
-//             projectId: "audiophile-eff2c",
-//             storageBucket: "audiophile-eff2c.appspot.com",
-//             messagingSenderId: "141435951049",
-//             appId: "1:141435951049:web:6308bd4b9fe95fb49bba18",
-//             measurementId: "G-LKVP0JH4YH"
-//         };
-//         const app = initializeApp(firebaseConfig);
-
-//     }
-// })
-
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-//VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE VUE 
-
-
+// Initiate Firebase Instance
 const firebaseConfig = {
     apiKey: "AIzaSyDRVQ7r6TGsQhZGvVIXws7y5PTPqlvC2yo",
     authDomain: "audiophile-eff2c.firebaseapp.com",
@@ -49,19 +18,23 @@ const firebaseConfig = {
     measurementId: "G-LKVP0JH4YH"
 };
 const app = initializeApp(firebaseConfig);
-// import {} from "../scripts/firebase.js"
-let files
-let nameBox = document.getElementById('songTitle'),
-    upload = document.getElementById('upload'),
+let files,
+    songTitle = document.getElementById('songTitle'),
+    artist = document.getElementById('artist'),
+    ext= document.getElementById('extension'),
     pbar = document.getElementById('pbar'),
-    retrieve = document.getElementById('retrieve'),
+    pbarContainer = document.getElementById('pbarContainer'),
+    upload = document.getElementById('upload'),
     player = document.getElementById('music'),
-    extlab = document.getElementById('extlab'),
+    retrieve = document.getElementById('retrieve'),
+    filename =document.getElementById('filename'),
+    musicSelection = document.getElementById("musicList"),
     reader = new FileReader()
 
 
-// || DRAG AND DROP || DRAG AND DROP || DRAG AND DROP || //
-
+//////////////////////
+// Drag and Drop JS //
+//////////////////////
 document.querySelectorAll(".drop-input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
   dropZoneElement.addEventListener("click", (e) => {
@@ -81,57 +54,24 @@ document.querySelectorAll(".drop-input").forEach((inputElement) => {
     });
   });
   dropZoneElement.addEventListener("drop", (e) => {
-    e.preventDefault();
-    const datatransfer = e.dataTransfer;
-    files = datatransfer.files;
-    var extention = GetFileExt(files[0])
+    e.preventDefault(); // Prevent Event's Auto
+    const data = e.dataTransfer; //Save event's file
+    files = data.files; // Assign data's files to file
+
+    var extension = GetFileExt(files[0]) // Get File extension
     var name = GetFileName(files[0])
-    nameBox.value=name
-    extlab.innerHTML = extention;
-    reader.readAsDataURL(files[0])
+    filename.innerHTML=name // This functions actaully no need le is just to get extension
+    ext.innerHTML =extension;
+    reader.readAsDataURL(files[0])//Read the current file as a URL
   });
 });
-
-// const initApp = () => {
-//     const droparea = document.querySelector('.droparea');
-//     const active = () => droparea.classList.add("green-border");
-//     const inactive = () => droparea.classList.remove("green-border");
-//     const prevents = (e) => e.preventDefault();
-//     ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evtName => {
-//         droparea.addEventListener(evtName, prevents);
-//     });
-//     ['dragenter', 'dragover'].forEach(evtName => {
-//         droparea.addEventListener(evtName, active);
-//     });
-//     ['dragleave', 'drop'].forEach(evtName => {
-//         droparea.addEventListener(evtName, inactive);
-//     });
-//     droparea.addEventListener("drop", handleDrop);
-    
-//     droparea.addEventListener("click", (e) => {
-//         ;
-//     });
-
-// }
-
-// document.addEventListener("DOMContentLoaded", initApp);
-
-// const handleDrop = (e) => {
-//     const datatransfer = e.dataTransfer;
-//     files = datatransfer.files;
-//     var extention = GetFileExt(files[0])
-//     var name = GetFileName(files[0])
-//     nameBox.value=name
-//     extlab.innerHTML = extention;
-//     reader.readAsDataURL(files[0])
-// }
-// || DRAG AND DROP || DRAG AND DROP || DRAG AND DROP || //
-
 reader.onload = function(){
     music.src = reader.result;
 }
 
-// Dropped file and get info from file
+///////////////////////
+/// GET FILE NAMES ////
+///////////////////////
 function GetFileExt(file){
     var temp = file.name.split('.');
     var ext = temp.slice(temp.length-1,temp.length);
@@ -143,72 +83,102 @@ function GetFileName(file){
     return fname
 }
 
-// Upload File
-async function UploadProcess(){ 
-    var fileToUpload =files[0];
-    var fileToUploadName = nameBox.value +  extlab.innerHTML;
-    if(!validateName()){
+///////////////////////
+///// UPLOAD FILE /////
+///////////////////////
+
+function validateName(){
+        var regex=/[\.#$\[]]/
+        return !(regex.test(songTitle.value+ ' - ' +artist.value ));
+    }
+
+async function UploadProcess(){
+    if(!files){
+        alert("Please Upload A File")
+        return
+    }  
+    var fileToUpload = files[0]
+    var fileToUploadName = songTitle.value + ' - ' +artist.value;
+     if(!validateName()){
         alert('Name cannot contain "[.#$[]]" ')
+        return;
+    }
+    else if(songTitle.value == '' || artist.value =='' ){
+        alert('Please Enter Song Title and Artist" ')
         return;
     }
     const metaData = {
         contentType: fileToUpload.type
     }
     const storage = getStorage();
-    const storageRef =sRef(storage, "users/" + id +"/"+ fileToUploadName); // To be updated when i can dynamically get the userid (prolly PHP sessions)
+    const storageRef =sRef(storage, "users/" + id +"/"+ fileToUploadName);
     const UploadTask = uploadBytesResumable(storageRef,fileToUpload,metaData);
-    console.log(UploadTask)
     UploadTask.on('state-changed',(snapshot)=>{
         var progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
-        pbar.innerHTML ='Upload' + progress + "%"
+        pbarContainer.style.visibility="visible"
+        pbar.innerHTML ='Upload ' + progress.toFixed(2) +  "%"
         pbar.style.width= progress+"%"
     },
     (error)=>{
-        alert("error: File failed to upload");
+        alert("Upload Failed Please Try Again"); // Failed to upload Errror
     },
     ()=>{
-        getDownloadURL(UploadTask.snapshot.ref).then((downloadURL)=>{
+        getDownloadURL(UploadTask.snapshot.ref)
+        .then((downloadURL)=>{
             SaveURLtoRealTimeDB(downloadURL);
-            console.log(downloadURL)})
-            .catch(err=>{console.log("WHNY CANNOT UPLOAD")})
+            })
+        .catch(err=>{
+                alert("Upload Failed Please Try Again")
+            })
             });
     }
-    
-// Save to REALTIME DB
-let filesrc = ''
+
+/////////////////////////////////
+///// SAVE FILE TO REALTIME /////
+/////////////////////////////////
 const realdb = getDatabase();
     // Function REALTIME DATABASE
     function SaveURLtoRealTimeDB(URL){
-        var name = nameBox.value;
+        var name = songTitle.value + " - "+ artist.value;
         var ext =extlab.innerHTML
         
         set(ref(realdb,"users/" + id  +"/"+ name),{
             musicName:(name + ext),
             musicURL:URL
         })
-    }
-
-function GetUrlfromRealTimeDB(){
-        var name =nameBox.value; // Get name of file
-        var dbRef =ref(realdb); // Refer to realtime DB
-        get(child(dbRef, "users/" + id  +"/"+ name)) // Get the file link
-        .then((snapshot)=>{
-            if(snapshot.exists()){ // if such a file link exist
-                var musicURL=snapshot.val().musicURL
-                console.log('im in')
-                var source = document.getElementById('source')
-                console.log(source)
-                source.src= musicURL // change the URL
-                filesrc = musicURL
-                player.load()}})
+        .then(res=>{
+            alert('Upload Complete')
+            getAllData()
+        })
         .catch(err=>{
-            console.log(err.message)
+            alert('Upload Failed')
         })
     }
-        
-function validateName(){
-        var regex=/[\.#$\[]]/
-        return !(regex.test(nameBox.value));
+///////////////////////////////////////
+///// RETRIEVE ALL FROM REALTIME //////
+///////////////////////////////////////
+function getAllData(){
+        var dbRef =ref(realdb); // Refer to realtime DB
+        get(child(dbRef,"users/" + id ))
+        .then((snapshot)=>{
+            var musicList = [];
+            snapshot.forEach(childSnapshot=>{
+                musicList.push(childSnapshot.val());
+            });
+            addToMusicList(musicList)
+        })
+        .catch(err=>{
+            alert('Failed to retrieve information please try again')
+        })
+}
+///////////////////////////////////////////
+/////////// Create A DROPDOWN /////////////
+///////////////////////////////////////////
+function addToMusicList(musicData){
+    musicSelection.innerHTML =""
+    for(let info of musicData){
+        musicSelection.innerHTML += "<option value='" + info.musicURL+ "' > "+ info.musicName+"</option>"
     }
-    retrieve.onclick=GetUrlfromRealTimeDB;
-    upload.onclick=UploadProcess;
+}
+upload.addEventListener('click',UploadProcess)
+
